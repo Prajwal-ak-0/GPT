bfd3c0e7dfed6978ee0d8e0eddb46c5b817b92e2
Prajwal-ak-0 <prajwalhassan7@gmail.com>
2023-04-26 08:54:56 +0530

    Deployed

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit=async (e)=>{
 
   //fetching the data from server
 
-  const response=await fetch('http://localhost:5000',{
+  const response=await fetch('https://gpt-81sf.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
